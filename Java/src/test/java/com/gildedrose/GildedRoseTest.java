ddf9ada04bc45ea06f4b8e9b04da9f6aaf05d4d4
Bongsu Lee <respectedb@gmail.com>
2023-06-15 01:33:04 +0900

    판매기간 초과 감소

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -19,5 +19,19 @@ class GildedRoseTest {
         gildedRoseService.updateQuality(items);
         assertEquals(9, item.getQuality());
     }
+    
+    @Test
+    void 판매기한초과_퀄리티_2배_감소() {
+        List<Item> items = new ArrayList<>();
+        Item item = Item.builder().
+                name(Constants.ETC).
+                sellIn(0).
+                quality(10).
+                build();
+        items.add(item);
+
+        gildedRoseService.updateQuality(items);
+        assertEquals(8, item.getQuality());
+    }
 
 }

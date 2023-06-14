95ac51909b9c7bb29eb616dfca9b9fd4ce13e310
Bongsu Lee <respectedb@gmail.com>
2023-06-15 01:32:31 +0900

    하루마다 퀄리티 감소

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,12 +7,17 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class GildedRoseTest {
 
     @Test
-    void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
-        GildedRose app = new GildedRose(items);
-        app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
-        System.out.println("TEST");
+    void 하루마다_퀄리티_감소() {
+        List<Item> items = new ArrayList<>();
+        Item item = Item.builder().
+                name(Constants.ETC).
+                sellIn(10).
+                quality(10).
+                build();
+        items.add(item);
+
+        gildedRoseService.updateQuality(items);
+        assertEquals(9, item.getQuality());
     }
 
 }
